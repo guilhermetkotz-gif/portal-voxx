ad858a0257cd2813753feacd91dabab0608b19c9
Guilherme Tkotz <guilherme.tkotz@voxxmarketing.com.br>
2026-01-06 19:13:21 +0000

    File changes

--- src/pages/GerenciarAcessos.jsx
+++ src/pages/GerenciarAcessos.jsx
@@ -77,7 +77,7 @@ export default function GerenciarAcessos({ user }) {
     cliente_usuario: 'Cliente Usuário'
   };
 
-  if (!user || (user.tipo_usuario !== 'voxx_admin' && user.tipo_usuario !== 'voxx_manager')) {
+  if (!user || (user.role !== 'admin' && user.tipo_usuario !== 'voxx_admin' && user.tipo_usuario !== 'voxx_manager')) {
     return (
       <Card className="p-8 text-center">
         <Shield className="w-12 h-12 text-red-500 mx-auto mb-4" />
