7b921b376f7307f71fc1ef34a06da10dbc4ed469
Guilherme Tkotz <guilherme.tkotz@voxxmarketing.com.br>
2026-01-07 04:35:09 +0000

    File changes

--- src/pages/GerenciarAcessos.jsx
+++ src/pages/GerenciarAcessos.jsx
@@ -165,14 +165,19 @@ export default function GerenciarAcessos({ user }) {
       </div>
 
       {/* Search */}
-      <div className="relative">
-        <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-slate-400" />
-        <Input
-          placeholder="Buscar por nome, email ou cliente..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="pl-10"
-        />
+      <div className="flex gap-2">
+        <div className="relative flex-1">
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-slate-400" />
+          <Input
+            placeholder="Buscar por nome, email ou cliente..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="pl-10"
+          />
+        </div>
+        <Button variant="outline" onClick={() => refetchUsuarios()}>
+          Atualizar
+        </Button>
       </div>
 
       {/* Tabs */}
