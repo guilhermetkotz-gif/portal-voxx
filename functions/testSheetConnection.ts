1f3b18c8a740e1c1dc7c51713f9fcc69a99a4111
Guilherme Tkotz <guilherme.tkotz@voxxmarketing.com.br>
2026-01-14 14:46:37 +0000

    File changes

--- functions/testSheetConnection.ts
+++ functions/testSheetConnection.ts
@@ -1,7 +1,7 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
 const SPREADSHEET_ID = '1aweubWBZdD71YvmBnDbq0xA6BUZCjL6_iuqmE2L9YA8';
-const SHEET_NAME = 'Sheet1'; // Alterado para a primeira aba da nova planilha
+const SHEET_NAME = 'Planilha1';
 
 Deno.serve(async (req) => {
     console.log('=== TESTE CONEXÃO SHEETS ===');
