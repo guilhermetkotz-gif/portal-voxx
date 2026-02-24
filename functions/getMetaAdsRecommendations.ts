6ff5ed2e3b0723c083067b13da202ac8baf53092
Guilherme Tkotz <guilherme.tkotz@voxxmarketing.com.br>
2026-02-24 21:40:20 +0000

    File changes

--- functions/getMetaAdsRecommendations.ts
+++ functions/getMetaAdsRecommendations.ts
@@ -1,6 +1,6 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
-// Motor de Recomendações Voxx Meta Ads - v1.0.1
+// Motor de Recomendações Voxx Meta Ads
 const CONFIG = {
   "version": "1.0.0",
   "investment_buckets": [
