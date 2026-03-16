986b9c8286bcf2b9a2a9d6807cce66063ef1f150
Guilherme Tkotz <guilherme.tkotz@voxxmarketing.com.br>
2026-03-16 19:56:32 +0000

    File changes

--- src/components/monitoramento/RadarTable.jsx
+++ src/components/monitoramento/RadarTable.jsx
@@ -10,7 +10,8 @@ import {
   Activity,
   Lightbulb,
   RefreshCw,
-  User
+  User,
+  AlertCircle
 } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import ResponsavelCellMeta from '@/components/metaads/ResponsavelCellMeta';
